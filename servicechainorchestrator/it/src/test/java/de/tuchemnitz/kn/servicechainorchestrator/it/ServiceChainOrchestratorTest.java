787321a959c8ab11999701e854118b9f3ffa21b0
Hanif Kukkalli <kukkalli@outlook.com>
2021-04-12 03:01:57 +0200

    comment unused import

--- servicechainorchestrator/it/src/test/java/de/tuchemnitz/kn/servicechainorchestrator/it/ServiceChainOrchestratorTest.java
+++ servicechainorchestrator/it/src/test/java/de/tuchemnitz/kn/servicechainorchestrator/it/ServiceChainOrchestratorTest.java
@@ -12,7 +12,7 @@ import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
 
 import org.junit.Assert;
-import org.junit.Ignore;
+// import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
